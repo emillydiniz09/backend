f1567306a25ffa6f3310aff5b9097fbfb59181f1
emillydiniz09 <emilly.weingartner@escola.pr.gov.br>
2026-03-02 10:51:31 -0300

    Add files via upload

---
+++ idade.js
@@ -0,0 +1,9 @@
+let idade = 17; 
+let dataNascimento = 2009;
+
+//console.log ("Minha idade é " + idade);
+//console.log ("Meu ano de nascimento é: " + dataNascimento);
+console.log (`Minha idade é: ${idade} e meu ano de nascimento é: ${dataNascimento}` );
+
+// O que fazer?
+// alterar as aspas duplas para crasce
