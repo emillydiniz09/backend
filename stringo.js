ea97a887e742f8641e8b928422af4fe6c9101d2e
emillydiniz09 <emilly.weingartner@escola.pr.gov.br>
2026-03-10 08:44:37 -0300

    Create stringo.js

---
+++ stringo.js
@@ -0,0 +1,12 @@
+console.log("ENDEREÇO COMPLETO");
+
+let rua = "Rua de Flores";
+let numero = 9851;
+let cidade = "Curitiba";
+let cep = "80010-000";
+
+let endereçoCompleto = rua + " , " + cidade +  " - " + cep;
+
+console.log(endereçoCompleto);
+
+//Adicione informações adicionais
