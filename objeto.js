0f3b11625e4e4ce92d0c744d74de1e13365d0dbd
emillydiniz09 <emilly.weingartner@escola.pr.gov.br>
2026-04-27 11:53:09 +0000

    versao1

---
+++ objeto.js
@@ -0,0 +1,11 @@
+const bancoAutomotivo = {
+material: "Couro Natural",
+cor: "Caramelo",
+tamanho: "Padrão",
+concluido: false
+}
+
+console.log("Material escolhido:", bancoAutomotivo.material);
+
+bancoAutomotivo.concluido = true;
+console.log(bancoAutomotivo);
